95e3bf87c2ce357fbb3122152284aa4b2fb4bcda
sophie liu <sophieliu@hotmail.fr>
2018-08-20 16:15:13 +0200

    added controller

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,26 @@
+class ToolsController < ApplicationController
+  def index
+    @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.find
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
