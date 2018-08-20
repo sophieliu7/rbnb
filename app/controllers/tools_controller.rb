e57c29a62603f2ae2f8c9dd05913ec6b0ff11036
sophie liu <sophieliu@hotmail.fr>
2018-08-20 16:40:52 +0200

    added controller for Tools

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -12,15 +12,34 @@ class ToolsController < ApplicationController
   end
 
   def create
-    @tool = Tool.find
+    @tool = Tool.new(tool_params)
+    @tool.user = current_user
+    if @tool.save!
+      redirect_to tools_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @tool = Tool.find(params[:id])
   end
 
   def update
+    @tool = Tool.find(params[:id])
+    if @tool.update!(tool_params)
+      redirect_to tools_path
+    else
+      render :update
+    end
   end
 
   def destroy
+  end
+
+  private
+
+    def tool_params
+    params.require(:tool).permit(:name, :price_per_day, :description, :place, )
   end
 end
