2f73298dd410c5d710cf6623bffa4b046b0fe659
sophie liu <sophieliu@hotmail.fr>
2018-08-20 15:18:38 +0200

    added User valdiation, added Tool model and validation

---
+++ app/models/tool.rb
@@ -0,0 +1,17 @@
+class Tool < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+
+  validates :price_per_day, presence: true
+  validates :price_per_day, numericality: { greater_than_or_equal_to: 0 }
+
+  # price_regex = /^\d+(\.\d{2})?$/
+  # validates :price_per_day, :format => {  :with => price_regex  }
+  # validates :price_per_day, format: { with: regex , message: "only 2 decimals"}
+
+  validates :price_per_day, :presence => true
+
+  validates :description, presence: true
+  validates :place, presence: true
+  validates :user, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_095443) do
+ActiveRecord::Schema.define(version: 2018_08_20_131307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tools", force: :cascade do |t|
+    t.string "name"
+    t.float "price_per_day"
+    t.text "description"
+    t.string "place"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tools_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +34,11 @@ ActiveRecord::Schema.define(version: 2018_08_20_095443) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tools", "users"
 end
