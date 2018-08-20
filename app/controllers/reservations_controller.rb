25dae1dc6b0ded29f97c90c34d5f4fe8a8387d65
Patrick Thimon <pt009@yahoo.fr>
2018-08-20 17:05:29 +0200

    pending reservation controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,20 @@
+class ReservationsController < ApplicationController
+  before_action:
+
+def index
+  @reservation = Reservation.current_user
+end
+
+def new
+  @reservation = Reservation.new
+  reservation.user = current_user
+
+end
+
+def create
+end
+
+def destroy
+end
+
+end
