12a5bceb88d5f388d8223cf57780df7729fc20e0
Patrick Thimon <pt009@yahoo.fr>
2018-08-21 10:25:09 +0200

    db seed & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_135312) do
+ActiveRecord::Schema.define(version: 2018_08_20_165542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2018_08_20_135312) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.string "sub_category"
     t.index ["user_id"], name: "index_tools_on_user_id"
   end
 
