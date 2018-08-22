d085bfb8ccb5d9a9b8ad288653a3c00b86031d29
sophie liu <sophieliu@hotmail.fr>
2018-08-22 13:07:32 +0200

    changed the typo

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Welcome !", "Let's thinker together"],
+    strings: ["Welcome !", "Let's tinker together"],
     typeSpeed: 70,
     loop: true
   });
