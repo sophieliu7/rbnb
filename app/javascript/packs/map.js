c0b79212bf2a6e30b75108c2b509addb54b952e3
benoit gallou <benoit.gallou@gmail.com>
2018-08-22 13:16:46 +0200

    maj map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -4,7 +4,6 @@ const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
-  alert(markers.lat);
   map.addMarkers(markers);
   if (markers.length === 0) {
     map.setZoom(2);
