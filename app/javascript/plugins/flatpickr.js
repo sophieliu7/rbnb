0a7210a9b126226a49cc4ea36bd06661ead70582
Patrick Thimon <pt009@yahoo.fr>
2018-08-23 15:10:09 +0200

    flatpickr disable added

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,12 +4,10 @@ import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 // flatpickr(".datepicker", {})
 
 const reservationForm = document.getElementById('form-flatpickr');
-console.log(reservationForm);
-console.log(dataset.reservations);
 const reservations = JSON.parse(reservationForm.dataset.reservations);
 
 flatpickr(".datepicker", {
   minDate: "today",
-  'disable': reservations,
+  disable: reservations,
   dateFormat: "Y-m-d"
 })
