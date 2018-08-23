c2c9833e69ee4d309d55987197fa7aaa9b1960e3
Clara Besnard <clarabesnard@gmail.com>
2018-08-23 21:14:09 +0200

    commit de sauvegarde

--- app/javascript/components/tab.js
+++ app/javascript/components/tab.js
@@ -1,14 +1,4 @@
-function showTab() {
-
-  const active_tabs = document.querySelectorAll(".nav-link");
-  const list = document.querySelector("ul")
-
-  active_tabs.forEach((tab) => {
-    tab.addEventListener("click", (event) => {
-      list.insertAdjacentHTML('afterend', "coucou")
-    })
-  })
-
-}
-
-export { showTab };
+// $('#nav-tabs a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
