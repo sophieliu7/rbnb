492dc7b99c65ebfe8878a925f319a61aba511d0d
sophie liu <sophieliu@hotmail.fr>
2018-08-23 16:41:40 +0200

    fix bug

--- app/javascript/components/reservation_confirmation.js
+++ app/javascript/components/reservation_confirmation.js
@@ -1,13 +1,15 @@
 function ConfirmInfo() {
-  const start_date = document.querySelector(".date-picker");
+  const start_date = document.getDocumentById("reservation_start_date");
+  const duration = document.getDocumentById("reservation_duration");
+
   console.log(start_date);
-  // const pricePerDay = parseInt(document.querySelector(".price-per-day").innerText);
-  // console.log(pricePerDay);
-  // let days = 0;
+
   let price = 0;
   let totalPrice = 0;
+
   start_date.addEventListener("blur", (event) => {
     console.log(start_date)
+    price.insertAdjacentHTML("", `${totalPrice}€`);
 
   // dates.forEach((date) => {
   //   date.addEventListener("blur", (event) => {
@@ -19,6 +21,7 @@ function ConfirmInfo() {
   //     }
   //   });
   // });
+});
 }
 
 
