4589cbb38eac523bf1df149b3adb4b24eef8d4d2
sophie liu <sophieliu@hotmail.fr>
2018-08-23 17:34:27 +0200

    clean

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :reservation
-  validates: :feedback,  :presence :true
-  validates: :rating, :inclusion => { :in => 1..5 }, :presence :true
-  validates: :reservation_id, :presence :true
+  validates :feedback,  presence: true
+  validates :rating, :inclusion => { :in => 1..5 }, presence: true
+  validates :reservation_id, presence: true
 end
